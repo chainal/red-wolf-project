b998c3b77d5432bd255d46ff251b9cccbece9f9e
chainal <chainal777@gmail.com>
2025-12-17 00:22:12 +0800

    add Datetime and Geopoint in DB

--- rwolf-server/src/bin/api_test.rs
+++ rwolf-server/src/bin/api_test.rs
@@ -4,13 +4,17 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Deserialize, Serialize)]
 struct CreateUserPosition {
     user: String,
+    lng: f64,
+    lat: f64,
 }
 
 #[tokio::main]
 async fn main() {
     let client = Client::new();
     let body = CreateUserPosition {
-        user: "liuchuang".to_string(),
+        user: "sakura".to_string(),
+        lng: 116.0,
+        lat: 38.1,
     };
     let response = client.post("http://127.0.0.1:3000/api/userposition")
         .json(&body)
--- rwolf-server/src/main.rs
+++ rwolf-server/src/main.rs
@@ -5,7 +5,7 @@ use axum::extract::State;
 use axum::{Json, Router};
 use axum::http::StatusCode;
 use axum::routing::{get, post};
-use mongodb::bson::doc;
+use mongodb::bson::{doc, DateTime};
 use mongodb::{Client, Collection};
 use mongodb::bson::oid::ObjectId;
 use mongodb::results::InsertOneResult;
@@ -14,9 +14,12 @@ use tower_http::trace::TraceLayer;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 
+const MONGO_URL: &str = "mongodb://127.0.0.1:27017/";
+const POINT_TYPE: &str = "Point";
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let db_connection = std::env::var("DATABASE_URL").unwrap_or_else(|_|"mongodb://127.0.0.1:27017/".to_string());
+    let db_connection = std::env::var("DATABASE_URL").unwrap_or_else(|_|MONGO_URL.to_string());
     let client = Client::with_uri_str(db_connection).await?;
     // pinging database
     client.database("rwolf").run_command(doc! {"ping": 1}).await?;
@@ -42,9 +45,12 @@ async fn post_position(
     State(db): State<Collection<UserPosition>>,
     Json(input): Json<CreateUserPosition>,
 ) -> Result<Json<InsertOneResult>, (StatusCode, String)> {
+    let datetime = DateTime::now();
     let user_postiton = UserPosition {
         id: ObjectId::new(),
         user: input.user,
+        create_at: datetime,
+        location: GeoPoint::new(input.lng, input.lat),
     };
     let result = db.insert_one(user_postiton).await.map_err(internal_error)?;
     Ok(Json(result))
@@ -61,9 +67,87 @@ struct UserPosition {
     #[serde(rename = "_id")]
     id: ObjectId,
     user: String,
+    #[serde(rename = "createtime")]
+    create_at: DateTime,
+    location: GeoPoint,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct GeoPoint {
+    #[serde(rename = "type")]
+    geo_type: String,
+    coordinates: [f64; 2],
+}
+
+impl GeoPoint {
+    fn new(lng: f64, lat: f64) -> Self {
+        Self {
+            geo_type: POINT_TYPE.to_string(),
+            coordinates: [lng, lat],
+        }
+    }
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 struct CreateUserPosition {
     user: String,
+    lng: f64,
+    lat: f64,
+}
+
+#[cfg(test)]
+mod tests {
+    use futures::StreamExt;
+    use mongodb::{Client, Collection};
+    use mongodb::bson::doc;
+    use mongodb::bson::oid::ObjectId;
+    use crate::{UserPosition, MONGO_URL};
+
+    #[tokio::test]
+    async fn list_records() {
+        let client = Client::with_uri_str(MONGO_URL).await.unwrap();
+        let collection: Collection<UserPosition> = client.database("rwolf").collection("user_positions");
+        let num = collection.count_documents(doc! {}).await;
+        if let Ok(num) = num {
+            println!("total {} documents", num);
+        }
+        let mut cursor = collection.find(doc! {}).await.unwrap();
+        let mut scan_num = 0;
+        while let Some(result) = cursor.next().await {
+            if scan_num >= 1000 {
+                break;
+            }
+            if let Ok(result) = result {
+                println!("{:?}", result);
+            }
+            scan_num += 1;
+        }
+    }
+
+    #[tokio::test]
+    async fn delete_records() {
+        let client = Client::with_uri_str(MONGO_URL).await.unwrap();
+        let collection: Collection<UserPosition> = client.database("rwolf").collection("user_positions");
+        let id = ObjectId::parse_str("69417f7df39c2647a36fb0a7").unwrap();
+        let result = collection.delete_one(doc! {"_id": id}).await;
+        match result {
+            Ok(result) => {
+                println!("delete success {:?}", result);
+            },
+            Err(e) => {
+                println!("delete error {}", e);
+            }
+        }
+    }
+
+    // DO NOT DELETE
+    // #[tokio::test]
+    // async fn create_geometry_index() {
+    //     let client = Client::with_uri_str(MONGO_URL).await.unwrap();
+    //     let collection: Collection<UserPosition> = client.database("rwolf").collection("user_positions");
+    //     let index = IndexModel::builder()
+    //         .keys(doc! {"location": "2dsphere"})
+    //         .build();
+    //     collection.create_index(index).await.unwrap();
+    // }
 }
